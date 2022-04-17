8419e1eb00264ac6cd5e19b490c13a0763e96fe6
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-17 17:53:13 -0400

    added a new solution

--- 7kyu/shortest-word.js
+++ 7kyu/shortest-word.js
@@ -4,3 +4,8 @@
 function findShort(s){
     return  s.split(" ").reduce((a,b) => a.length<=b.length ? a : b ).length
   }
+
+//Another solution
+function findShort(s){
+    return Math.min.apply(null, s.split(' ').map(w => w.length));
+  }
