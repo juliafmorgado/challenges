56f5f0495bdc3f503437c77946a91196dbbea7d4
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-19 12:10:16 -0400

    added new kata

---
+++ 7kyu/square-nb.js
@@ -0,0 +1,12 @@
+// Given an integral number, determine if it's a square number:
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
