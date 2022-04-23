788ccc1c7cd580fe088d4d1173ca2056e0f82df1
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-23 14:26:18 -0400

    added new kata

---
+++ 7kyu/credit-card-mask.js
@@ -0,0 +1,9 @@
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+
+//Solution
