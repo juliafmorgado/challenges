56e389fcb6511209a7b449d79722a062b5c537ad
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-23 20:40:59 -0400

    modified into ternary operator

--- 7kyu/credit-card-mask.js
+++ 7kyu/credit-card-mask.js
@@ -8,7 +8,7 @@
 
 //Solution
 function maskify(cc) {
-    return cc.split('').map((letter, index) => index < cc.length-4 ? '#':letter).join('')
+    return cc.split('').map((letter, index) => index < cc.length-4 ? '#' : letter).join('')
     }
 
 //Another solution
