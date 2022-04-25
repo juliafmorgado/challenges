16ce3d497c343d549f9f852396cb7ab2dade43ec
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-24 20:32:00 -0400

    added my solution

--- 7kyu/list-filtering.js
+++ 7kyu/list-filtering.js
@@ -4,3 +4,8 @@
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+//My solution
+function filter_list(l) {
+    return l.filter(Number.isFinite);
+  }
