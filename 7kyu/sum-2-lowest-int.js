328696fb09bc77983ef7d446a617cbe1a3de8817
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-27 18:38:08 -0400

    added my solution

--- 7kyu/sum-2-lowest-int.js
+++ 7kyu/sum-2-lowest-int.js
@@ -2,3 +2,10 @@
 
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 
+//My solution
+function sumTwoSmallestNumbers(numbers) {  
+    return numbers
+    .sort((a,b) => a-b)
+    .slice(0, 2)
+    .reduce((acc, c) => acc + c, 0)
+  }
