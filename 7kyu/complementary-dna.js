268d1966f0663d929321c44f358a16a7d6ee1cd5
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-27 18:26:27 -0400

    added new kata

---
+++ 7kyu/complementary-dna.js
@@ -0,0 +1,6 @@
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// Example:
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
