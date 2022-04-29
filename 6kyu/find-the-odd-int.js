aecca5b804f24c0a695fe2ff63b75dbbc08d99fb
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-29 12:20:48 -0400

    adding comments

--- 6kyu/find-the-odd-int.js
+++ 6kyu/find-the-odd-int.js
@@ -8,3 +8,23 @@
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+//My solution
+function findOdd(A) {
+    //runs two nested loops
+    for(let i = 0; i < A.length; i++) {
+      let count = 0;
+      
+      for (let j = 0; j < A.length; j++) {
+        if(A[i] == A[j]) {
+          count++;
+        }
+      }
+      
+      if (count % 2 != 0) {
+        return A[i];
+      }
+    }
+    
+    return -1;
+  }
