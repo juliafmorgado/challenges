2591d879b027a313b39c71c9bd88b565a416980b
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-18 15:58:28 -0400

    added new kata

---
+++ 7kyu/merge-two-arrays.js
@@ -0,0 +1,8 @@
+// Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+
+// Examples:
+
+// [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+// [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+
