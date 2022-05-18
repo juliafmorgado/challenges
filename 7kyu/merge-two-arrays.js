7e36678c01306054bb972ef87cf21adf7b303f08
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-18 15:59:12 -0400

    added my solution

--- 7kyu/merge-two-arrays.js
+++ 7kyu/merge-two-arrays.js
@@ -6,3 +6,13 @@
 
 // [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
 
+//My solution
+function mergeArrays(a, b) {
+  let c = a.concat(b)
+  let result = []
+  //loop through both arrays
+  for (let i=0; i<=c.length; i++){
+    result.push(a[i] , b[i])
+} //filter out the undefined values
+  return result.filter((e) => e!==(undefined))
+  }
