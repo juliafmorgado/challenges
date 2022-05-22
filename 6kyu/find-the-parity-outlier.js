051bb8d5dc9b92f16e22a6dbd6b5fa535fdbea76
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-22 12:46:21 -0400

    Updated solution

--- 6kyu/find-the-parity-outlier.js
+++ 6kyu/find-the-parity-outlier.js
@@ -6,3 +6,11 @@
 
 // [160, 3, 1719, 19, 11, 13, -21]
 // Should return: 160 (the only even number)
+
+//Solution
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
+
