b5abb1052dc42aa01d2017d736497d1e504dc1df
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-26 10:50:14 -0400

    Added new solution

--- 8kyu/petals.js
+++ 8kyu/petals.js
@@ -23,3 +23,20 @@ const phrases = [
 function howMuchILoveYou(n) {
      return phrases[(n - 1) % phrases.length] 
 }
+
+//New solution
+function howMuchILoveYou(nbPetals) {
+  if(nbPetals%6 === 1){
+    return "I love you"
+  } else if(nbPetals%6 === 2){
+    return "a little"
+  } else if(nbPetals%6 === 3){
+    return "a lot"
+  } else if(nbPetals%6 === 4){
+    return "passionately"
+  } else if(nbPetals%6 === 5){
+    return "madly"
+  } else {
+    return "not at all"
+  }
+}
