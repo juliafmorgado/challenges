e8d0d22032a2edfabdd2353b1babdd76c963b599
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-26 10:50:45 -0400

    Added another solution

--- 8kyu/petals.js
+++ 8kyu/petals.js
@@ -40,3 +40,21 @@ function howMuchILoveYou(nbPetals) {
     return "not at all"
   }
 }
+
+//New solution
+function howMuchILoveYou(nbPetals) {
+    switch ((nbPetals - 1) % 6) {
+        case 1:
+            return "a little";
+        case 2:
+            return "a lot";
+        case 3:
+            return "passionately";
+        case 4:
+            return "madly";
+        case 5:
+            return "not at all";
+        default:
+            return "I love you";
+    }
+}
