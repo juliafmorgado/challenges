f0fadc9240e0620eb3b76f6944678cc4a36cae83
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-26 10:49:45 -0400

    Added solution

--- 8kyu/petals.js
+++ 8kyu/petals.js
@@ -9,3 +9,17 @@
 // When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+//Solution
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
