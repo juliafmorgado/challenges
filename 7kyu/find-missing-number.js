5c34447226fd4858abf38f6aaf3ee1717eb00c83
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-28 10:05:27 -0400

    Included solution

--- 7kyu/find-missing-number.js
+++ 7kyu/find-missing-number.js
@@ -2,3 +2,11 @@
 
 // You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
 
+//Solution
+function missingNo(nums) {
+ for (let i=0; i<=100; i++) {
+   if (!nums.includes(i)) {
+    return i;
+   }
+}
+}
