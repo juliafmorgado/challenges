4483827db897b1df5e28556f931766d5f8226829
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-05-31 18:48:49 -0400

    Update exclamation-marks.js

--- 7kyu/exclamation-marks.js
+++ 7kyu/exclamation-marks.js
@@ -9,3 +9,24 @@
 // remove("Hi! Hi!! Hi!") === "Hi!!"
 // remove("Hi! !Hi! Hi!") === "!Hi!"
 
+//Solution
+
+function remove(string) {
+    // PREP
+    // (P)arameter:  a string of words
+    // (R)eturn: words without exactly one "!"
+    // (E)xamples: ("Hi! Hi!") => ""; ("Hi Hi! Hi!) => "Hi"
+    // (P)seuducode: 
+    // Make string iterable
+    let strArr = string.split(" ")
+    let newStrArr = []
+    for (let i = 0; i < strArr.length; i++) {
+        // if the str element has a number of "!" different than 1
+        if ((strArr[i].split("").filter(e => e == "!").length != 1)) {
+            // Add it to the new array
+            newStrArr.push(strArr[i])
+        }
+    }
+    let newStr = newStrArr.join(" ")
+    return newStr
+}
