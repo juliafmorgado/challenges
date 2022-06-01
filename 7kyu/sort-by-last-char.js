de78fc523b63588e133768693245a21be0766096
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-01 19:15:35 -0400

    Update sort-by-last-char.js

--- 7kyu/sort-by-last-char.js
+++ 7kyu/sort-by-last-char.js
@@ -13,3 +13,8 @@ function last (words) {
 function last(x){
   return x.split(" ").sort((a, b)=> a.slice(-1)>b.slice(-1));
 }
+
+//Another solution
+function last(x){
+  return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
