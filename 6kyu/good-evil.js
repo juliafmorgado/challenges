aa6d60c331c839248cda201fa90107f96cecabdf
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-03 20:44:55 -0400

    Update good-evil.js

--- 6kyu/good-evil.js
+++ 6kyu/good-evil.js
@@ -33,3 +33,17 @@
 // Output:
 // Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
 
+//Solution
+function goodVsEvil(good, evil) {  
+  var getWorth = function( side, worth ) {
+    return side.split(' ').reduce( function(result, value, index) { 
+      return result + (worth[index] * value);
+    }, 0);
+  }
+
+  var result = getWorth( good, [1,2,3,3,4,10] ) - getWorth( evil, [1,2,2,2,3,5,10] );
+
+  return result > 0 ? "Battle Result: Good triumphs over Evil" :
+         result < 0 ? "Battle Result: Evil eradicates all trace of Good" :
+                      "Battle Result: No victor on this battle field";
+}
