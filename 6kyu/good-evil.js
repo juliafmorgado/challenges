2697e9a082ecc7c3f9a0a963336de08c9b245a60
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-03 20:45:17 -0400

    Update good-evil.js

--- 6kyu/good-evil.js
+++ 6kyu/good-evil.js
@@ -47,3 +47,45 @@ function goodVsEvil(good, evil) {
          result < 0 ? "Battle Result: Evil eradicates all trace of Good" :
                       "Battle Result: No victor on this battle field";
 }
+
+//Another solution
+const goodWorth = [
+  1, // Hobbits
+  2, // Men
+  3, // Elves
+  3, // Dwarves
+  4, // Eagles
+  10, // Wizards
+]
+
+const evilWorth = [
+  1, // Orcs
+  2, // Men
+  2, // Wargs
+  2, // Goblins
+  3, // Uruk Hai
+  5, // Trolls
+  10, // Wizards
+]
+
+const calculateWorth = (points, force) =>
+  points
+    .split(' ')
+    .reduce(
+      (total, value, index) =>
+        force === 'good'
+          ? Number(total) + goodWorth[index] * Number(value)
+          : Number(total) + evilWorth[index] * Number(value),
+      0
+    )
+
+function goodVsEvil(good, evil) {
+  const goodTotalWorth = calculateWorth(good, 'good')
+  const evilTotalWorth = calculateWorth(evil, 'evil')
+
+  if (evilTotalWorth > goodTotalWorth)
+    return 'Battle Result: Evil eradicates all trace of Good'
+  else if (goodTotalWorth > evilTotalWorth)
+    return 'Battle Result: Good triumphs over Evil'
+  else return 'Battle Result: No victor on this battle field'
+}
