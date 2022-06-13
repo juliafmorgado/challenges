cbf58ab93e7cd2cc560587ff248ab889940a7033
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-13 11:27:42 -0400

    Update sums-of-parts.js

--- 6kyu/sums-of-parts.js
+++ 6kyu/sums-of-parts.js
@@ -22,3 +22,10 @@ function partsSums(ls) {
   }
   return result.reverse()
 }
+
+//Another solution
+function partsSums(ls) {
+    ls.unshift(0);
+    let sum = ls.reduce((p, c) => p + c, 0);
+    return ls.map(v => sum = sum - v);
+}
