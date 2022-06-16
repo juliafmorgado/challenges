b995180750ef1b1953949f47a4516b10fe5bd4b8
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-16 12:57:54 -0400

    Update xor-operator.js

--- 8kyu/xor-operator.js
+++ 8kyu/xor-operator.js
@@ -8,3 +8,8 @@
 // true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
 // Task
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+//Solution
+function xor(a, b) {
+  return a != b;
+}
