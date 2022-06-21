f6b4a61bfd16dc151c8fdf0adca5ce4e9c23e70d
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-20 20:40:38 -0400

    Create fixe-replace-dots.js

---
+++ 8kyu/fixe-replace-dots.js
@@ -0,0 +1,6 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
