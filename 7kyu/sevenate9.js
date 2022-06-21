56452770af5d20ba7b96e24bfc7d7074ec58c58f
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-21 16:10:23 -0400

    Update sevenate9.js

--- 7kyu/sevenate9.js
+++ 7kyu/sevenate9.js
@@ -2,3 +2,15 @@
 
 // "79712312" --> "7712312"
 // "79797"    --> "777"
+
+
+//Solution
+function sevenAte9(str) {
+  let arr = str.split('')
+  for (let i=0; i<= arr.length; i++){
+    if (arr[i]== '9' && arr[i-1]=='7' && arr[i+1]== '7'){
+      arr.splice(i, 1)
+    }
+  }
+  return arr.join('')
+}
