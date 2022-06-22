84e1ccdb3946e54f67bcf932b40146e3494d3e41
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-22 10:25:57 -0400

    Update roman-numerals-encoder.js

--- 6kyu/roman-numerals-encoder.js
+++ 6kyu/roman-numerals-encoder.js
@@ -16,3 +16,19 @@
 // D          500
 // M          1,000
 // Remember that there can't be more than 3 identical symbols in a row.
+
+//Solution
+function solution(number){
+  let result   = '',
+      decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+      roman    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+  decimals.map((value, index)=> {
+    while (number >= value) {
+      result += roman[index];
+      number -= value;
+    }
+  });
+  
+  return result;
+}
