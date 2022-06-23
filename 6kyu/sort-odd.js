6ca7fad5987d7a2a7e0c5eebd8500dc17c346e31
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-06-23 11:09:40 -0400

    Update sort-odd.js

--- 6kyu/sort-odd.js
+++ 6kyu/sort-odd.js
@@ -4,3 +4,9 @@
 // [7, 1]  =>  [1, 7]
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+//Solution
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+  return array.map((x) => x % 2 ? odd.shift() : x);
+}
