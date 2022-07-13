743df4f15175a7150b2b0c78932def9189bd4867
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-07-13 14:43:08 -0400

    Update array-plus-array.js

--- 8kyu/array-plus-array.js
+++ 8kyu/array-plus-array.js
@@ -6,6 +6,7 @@ function arrayPlusArray(arr1, arr2) {
     return arr1.concat(arr2).reduce((acc, c) => acc + c);
 }
 
+
 //Another solution
 function arrayPlusArray(arr1, arr2){
  return (...arr1,...arr2).reduce((acc,c) => acc + c)   
