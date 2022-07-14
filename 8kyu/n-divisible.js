0f71de6c144a6884eadea7444cc2f49a181629f8
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-07-13 20:49:31 -0400

    Update n-divisible.js

--- 8kyu/n-divisible.js
+++ 8kyu/n-divisible.js
@@ -5,3 +5,8 @@
 // 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+// Solution
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
