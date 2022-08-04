4eaa24f4e46e7e2f620944c2d5b1bd2215dba4c9
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-08-04 19:42:49 +0100

    Update square-every-digit.js

--- 7kyu/square-every-digit.js
+++ 7kyu/square-every-digit.js
@@ -2,6 +2,7 @@
 
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
+
 //Solution
 function squareDigits(num){
     return Number((""+num).split("").map(x => x * x).join(''))
