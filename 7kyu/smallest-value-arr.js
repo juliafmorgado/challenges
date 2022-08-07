d9f6706eaf0cd799a32721ad4a22c670bec5f18b
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-08-07 17:43:15 +0100

    Update smallest-value-arr.js

--- 7kyu/smallest-value-arr.js
+++ 7kyu/smallest-value-arr.js
@@ -4,3 +4,6 @@
 
 // min([1,2,3,4,5], 'value') // => 1
 // min([1,2,3,4,5], 'index') // => 0
+
+//Solution
+const min = (arr, toReturn) => toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
