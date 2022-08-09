b5be45fe7cd66902da19127482e47a2288969227
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-08-09 21:25:43 +0100

    Update palindrome.js

--- 8kyu/palindrome.js
+++ 8kyu/palindrome.js
@@ -1,4 +1,3 @@
 //Write a function that checks if a given string (case insensitive) is a palindrome.
 
-
 let isPalindrome = x => x.toLowerCase() === (x.split('').reverse().join('')).toLowerCase() ? true : false
