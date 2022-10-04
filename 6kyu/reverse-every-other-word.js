87e5a7c0755553b0a89fa70502750630466ae8ab
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-10-04 10:36:21 -0400

    Create reverse-every-other-word.js

---
+++ 6kyu/reverse-every-other-word.js
@@ -0,0 +1,4 @@
+// Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+//Solution
+const reverse=str=>str.trim().split(' ').map((e,i)=> i%2==0 ? e : e.split('').reverse().join('')).join(' ');
