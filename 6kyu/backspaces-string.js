ea051f5f8476c3421fd9a72d3a5321936ec3049b
Julia Furst Morgado <52685951+juliafmorgado@users.noreply.github.com>
2022-10-05 10:57:14 -0400

    Create backspaces-string.js

---
+++ 6kyu/backspaces-string.js
@@ -0,0 +1,13 @@
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+//Solution
+
+clean_string = s => s.split('').reduce((r, e) => e == '#' ? r.slice(0, -1) : r + e, '');
